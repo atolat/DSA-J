6bef82a51e64522f8699efb2ede7bb0c43230c7e
atolat <atolat@uci.edu>
2016-10-07 15:40:59 -0700

    Check shrink!

---
+++ src/StackArray/Main.java
@@ -0,0 +1,21 @@
+package StackArray;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Stack<Integer> myStack = new Stack<>();
+		myStack.push(1);
+		myStack.push(2);
+		myStack.push(3);
+		myStack.push(4);
+		myStack.push(5);
+		
+		System.out.println(myStack.pop());
+		System.out.println(myStack.pop());
+		System.out.println(myStack.pop());
+		System.out.println(myStack.pop());
+		System.out.println(myStack.pop());
+		System.out.println(myStack.pop());
+		
+	}
+}
---
+++ src/StackArray/Stack.java
@@ -0,0 +1,48 @@
+package StackArray;
+
+public class Stack<T> {
+
+	private T[] stack;
+	private int size=0;
+
+	//Constructor
+	public Stack() {
+		//Create an array of size 1
+		this.stack = (T[]) new Object[1];
+	}
+
+	public void push(T data) {
+		size++;
+		//Check size, if full, resize
+		if(this.stack.length == size){
+			resize(2*this.stack.length);
+		} 
+		
+		this.stack[size] = data;
+	}
+	
+	public T pop() {
+		if(size == 0){
+			System.out.println("Empty!");
+			return null;
+		} else {
+			
+			//check to shrink size...memory optimization
+//			if(size == this.stack.length/4){
+//				resize(this.stack.length/2);
+//			}
+//			
+			return this.stack[size--];
+		}
+	}
+
+	//Method to resize arrays
+	public void resize(int capacity) {
+		T[] resizedArray = (T[]) new Object[capacity];
+		for(int i = 0; i<this.stack.length;i++){
+			resizedArray[i] = this.stack[i];
+		}
+		this.stack = resizedArray;
+	}
+
+}
