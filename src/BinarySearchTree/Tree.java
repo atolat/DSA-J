c52b44ae1c19ae1ab8541fb2208fdc59530f333d
atolat <atolat@uci.edu>
2016-10-09 14:27:54 -0700

    BST-Node, Tree Interface

---
+++ src/BinarySearchTree/Tree.java
@@ -0,0 +1,16 @@
+package BinarySearchTree;
+
+//Interface to define all methods that must be implemented by BST/any tree in general
+public interface Tree<T> {
+
+	public void traverse();
+
+	public void insert(T data);
+
+	public void delete(T data);
+
+	public T getMax();
+
+	public T getMin();
+
+}
