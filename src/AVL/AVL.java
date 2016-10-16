72eef81489689fa71735bddd23011cda7288a394
atolat <atolat@uci.edu>
2016-10-15 17:48:12 -0700

    AVL insert works

--- src/AVL/AVL.java
+++ src/AVL/AVL.java
@@ -6,15 +6,59 @@ public class AVL implements Tree {
 
 	@Override
 	public void insert(int data) {
-		// TODO Auto-generated method stub
+		root = insert(root, data);
+	}
 
+	private Node insert(Node recurseNode, int data) {
+		// Check if root is null, tree is empty
+		if (recurseNode == null)
+			return new Node(data);
+		// Right
+		if (data > recurseNode.getData()) {
+			recurseNode.setRightNode(insert(recurseNode.getRightNode(), data));
+		}
+		// Left
+		else {
+			recurseNode.setLeftNode(insert(recurseNode.getLeftNode(), data));
+		}
+		recurseNode.setHeight(Math.max(height(recurseNode.getLeftNode()), height(recurseNode.getRightNode())) + 1);
+
+		recurseNode = checkForViolations(data, recurseNode);
+		return recurseNode;
+	}
+
+	private Node checkForViolations(int data, Node recurseNode) {
+		int balance = getBalanceFactor(recurseNode);
+
+		// For Case I, Double left heavy -- One Right Rotation
+		if (balance > 1 && data < recurseNode.getLeftNode().getData()) {
+			return rotateRight(recurseNode);
+		}
+
+		// For Case II, Double right heavy -- One Left Rotation
+		if (balance < -1 && data > recurseNode.getRightNode().getData()) {
+			return rotateLeft(recurseNode);
+		}
+
+		// Case III, Left Right Heavy -- Two Rotations
+		if (balance > 1 && data > recurseNode.getLeftNode().getData()) {
+			recurseNode.setLeftNode(rotateLeft(recurseNode.getLeftNode()));
+			return rotateRight(recurseNode);
+		}
+
+		// Case IV, Right Left Heavy -- Two Rotations
+		if (balance < -1 && data < recurseNode.getRightNode().getData()) {
+			recurseNode.setRightNode(rotateRight(recurseNode.getRightNode()));
+			return rotateLeft(recurseNode);
+		}
+		return recurseNode;
 	}
 
 	@Override
 	public void traverse() {
 		if (this.root == null)
 			return;
-		inOrderTrsverse(this.root);
+		inOrderTrasverse(this.root);
 	}
 
 	private int height(Node node) {
@@ -30,14 +74,14 @@ public class AVL implements Tree {
 
 	}
 
-	private void inOrderTrsverse(Node recurseNode) {
+	private void inOrderTrasverse(Node recurseNode) {
 		if (recurseNode.getLeftNode() != null)
-			inOrderTrsverse(recurseNode.getLeftNode());
+			inOrderTrasverse(recurseNode.getLeftNode());
 
 		System.out.println(recurseNode);
 
 		if (recurseNode.getRightNode() != null)
-			inOrderTrsverse(recurseNode.getRightNode());
+			inOrderTrasverse(recurseNode.getRightNode());
 	}
 
 	// Right Rotate
@@ -61,7 +105,7 @@ public class AVL implements Tree {
 		return tempLeftNode;
 
 	}
-	
+
 	// Left Rotate
 	private Node rotateLeft(Node node) {
 
@@ -77,7 +121,8 @@ public class AVL implements Tree {
 
 		// Update Heights
 		node.setHeight(Math.max(height(node.getLeftNode()), height(node.getRightNode())) + 1);
-		tempRightNode.setHeight(Math.max(height(tempRightNode.getLeftNode()), height(tempRightNode.getRightNode())) + 1);
+		tempRightNode
+				.setHeight(Math.max(height(tempRightNode.getLeftNode()), height(tempRightNode.getRightNode())) + 1);
 
 		// Return the new Root
 		return tempRightNode;
---
+++ src/AVL/Main.java
@@ -0,0 +1,21 @@
+package AVL;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Tree myAvl = new AVL();
+		
+		myAvl.insert(1);
+		myAvl.insert(2);
+		myAvl.insert(3);
+		myAvl.insert(4);
+		myAvl.insert(5);
+		myAvl.insert(10);
+		myAvl.insert(8);
+		myAvl.insert(7);
+		myAvl.insert(20);
+		myAvl.insert(50);
+		
+		
+	}
+}
